ba56e8ffb3e06b8bcea7e9fe5cc7c18ada8c638c
ThatsNotMe <mineflax@gmail.com>
2024-10-10 22:05:32 +0200

    Integrated Minesweeper.jar

--- minesweeper/app/controllers/HomeController.scala
+++ minesweeper/app/controllers/HomeController.scala
@@ -4,21 +4,16 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+import com.google.inject.Guice
+import de.htwg.se.minesweeper.aview.{TUI, MinesweeperGUI}
+import de.htwg.se.minesweeper.controller.Controller
+import de.htwg.se.minesweeper.util.StdInInputSource
+
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
+
 }
