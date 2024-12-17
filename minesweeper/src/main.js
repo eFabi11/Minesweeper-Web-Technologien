9c69e00e50e4231cba6d115e5fc02b4ff6127b62
ThatsNotMe <mineflax@gmail.com>
2024-12-17 17:42:23 +0100

    ToDo: add Measurement-ID

--- minesweeper/src/main.js
+++ minesweeper/src/main.js
@@ -1,3 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueGtag from 'vue-gtag';
+
+const app = createApp(App);
+
+app.use(VueGtag, {
+  config: { id: "G-XXXXXXXXXX" }, // Replace with your Measurement ID
+});
+
 createApp(App).mount('#app')
