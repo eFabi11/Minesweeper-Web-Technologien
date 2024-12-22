36b1ccdd817deddc45b655815a8a1bb00fcb0275
eFabi11 <fabian.eitel@yahoo.de>
2024-12-22 13:24:27 +0100

    vite

--- minesweeper/vite.config.js
+++ minesweeper/vite.config.js
@@ -6,31 +6,33 @@ export default defineConfig({
   define: {
     __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: true
   },
-  
   plugins: [vue()],
   build: {
-    outDir: 'public', // This is where the Vite build will output to
-    assetsDir: 'assets', // This is the folder for your assets (e.g., images, styles)
+    outDir: 'dist', // Standardordner für Build-Output
+    assetsDir: 'assets',
     assetsInclude: ['**/*.mp4'],
     rollupOptions: {
       input: {
-        main: 'main.js', // Your main entry JS file
+        main: 'main.js', // Dein Haupt-JavaScript-Einstiegspunkt
       },
     },
   },
   server: {
-    proxy: {
-      '/assets': {
-        target: 'http://localhost:9000',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/game': {
-        target: 'http://localhost:9000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+    port: process.env.PORT || 8000, // Nutzt dynamischen Port oder 3000 als Fallback
+    strictPort: true, // Stoppt den Server, wenn der Port belegt ist
+    host: '0.0.0.0', // Erlaubt externen Zugriff
+    // Proxy nur lokal aktivieren:
+    // proxy: {
+    //   '/assets': {
+    //     target: 'http://localhost:9000',
+    //     changeOrigin: true,
+    //     secure: false,
+    //   },
+    //   '/game': {
+    //     target: 'http://localhost:9000',
+    //     changeOrigin: true,
+    //     secure: false,
+    //   },
+    // },
   },
 });
-
