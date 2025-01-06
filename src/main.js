66d43482becc135ff484576b873e4f5881eabe5a
ThatsNotMe <mineflax@gmail.com>
2025-01-06 21:38:27 +0100

    something bad will happen

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import VueGtag from 'vue-gtag';
 
-const app = createApp(App);
-
-app.use(VueGtag, {
-  config: { id: "G-XXXXXXXXXX" }, // Replace with your Measurement ID
-});
-
-createApp(App).mount('#app')
+new Vue({
+    router,
+    store,
+    render: h => h(App)
+}).$mount('#app');
